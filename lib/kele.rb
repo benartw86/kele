ecf160bea46d739aaf25833bdd7851072bb93593
Ben <benartw86@yahoo.com>
2017-07-15 01:17:07 +0000

    Gemfile added

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,14 +1,19 @@
  require 'httparty'
+ require 'json'
  
 class Kele
-  include HTTPARTY
+  include HTTParty
   
-  def initialize(username, password)  
-    
-   response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {username: "username", password: 
-   password})
-   
-   raise "invalid email/password" if response.code != 200
-   
+  def initialize(username, password)
+     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { "username": username, "password": 
+     password })
+    raise "invalid email/password" if response.code != 200
+    @auth_token = response["auth_token"] 
+  end
+  
+  def get_me
+    url = "https://www.bloc.io/api/v1/users/me"
+    response = self.class.get(url, headers: { "authorization" => auth_token })
+    @user = JSON.parse(response.body)
   end
 end
