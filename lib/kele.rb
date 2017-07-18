4035abd5b58a6ef4d2c6e20284a559c56437713a
Ben <benartw86@yahoo.com>
2017-07-18 16:56:39 +0000

    Mentor availability method defined, hash changed to array

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,8 +4,8 @@
 class Kele
   include HTTParty
   
-  def initialize(username, password)
-     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { "username": username, "password": 
+  def initialize(email, password)
+     response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { "email": email, "password": 
      password })
     raise "invalid email/password" if response.code != 200
     @auth_token = response["auth_token"] 
@@ -13,7 +13,19 @@ class Kele
   
   def get_me
     url = "https://www.bloc.io/api/v1/users/me"
-    response = self.class.get(url, headers: { "authorization" => auth_token })
+    response = self.class.get(url, headers: { "authorization" => @auth_token })
     @user = JSON.parse(response.body)
+  end
+  
+  def get_mentor_availability(mentor_id)
+    url = "https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability"  
+    response = self.class.get(url, headers: { "authorization" => @auth_token })
+    available = []
+    @mentor_availability = JSON.parse(response.body).each do |time|
+      if time["booked"] == nil
+        available << time
+      end
+    end
+    available
   end
 end
