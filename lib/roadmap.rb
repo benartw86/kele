17de61329c55cbfefb8796ab4e27583b67c7fab5
Ben <benartw86@yahoo.com>
2017-07-19 23:01:49 +0000

    Methods for roadmap and checkpoint defined, put in their own module, error in testing get_checkpoint

---
+++ lib/roadmap.rb
@@ -0,0 +1,14 @@
+module Roadmap
+
+  def get_roadmap(roadmap_id)
+    url = "https://www.bloc.io/api/v1/roadmaps/#{roadmap_id}"
+    response = self.class.get(url, headers: { "authorization" => @auth_token })
+    @roadmap = JSON.parse(response.body)
+  end
+  
+  def get_checkpoint(checkpoint_id)
+    url = "https://www.bloc.io/api/v1/checkpoints/#{checkpoint_id}"  
+    response = self.class.get(url, headers: { "authorization" => @auth_token })
+    @checkpoint = JSON.parse(response.body)
+  end
+end
